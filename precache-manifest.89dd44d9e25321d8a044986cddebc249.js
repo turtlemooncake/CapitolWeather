6d09753a4a68bfa5609dc3c9ac108a525d71962e
turtlemooncake <vickitori109@gmail.com>
2021-01-11 13:02:35 -0600

    Updates

---
+++ precache-manifest.89dd44d9e25321d8a044986cddebc249.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67dcf55aed23eb83aac99caf77bb56f",
+    "url": "/CapitolWeather/index.html"
+  },
+  {
+    "revision": "5f861ea92235fc027c90",
+    "url": "/CapitolWeather/static/js/2.561f7560.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CapitolWeather/static/js/2.561f7560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6edbbd2e06eb4fd37f8",
+    "url": "/CapitolWeather/static/js/main.2cde1f20.chunk.js"
+  },
+  {
+    "revision": "446c63de61a892182cfe",
+    "url": "/CapitolWeather/static/js/runtime-main.aadada44.js"
+  }
+]);
